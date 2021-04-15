dbf10da6990788d55d53a6a00bbdd027333b138a
Fatih Çetinkaya <fatihxcetinkaya@hotmail.com>
2021-04-15 16:54:51 +0200

    Init_Bit

--- tutorial/crud-login-authentication-firebase/main.js
+++ tutorial/crud-login-authentication-firebase/main.js
@@ -12,28 +12,50 @@
 
  firebase.auth().onAuthStateChanged(function (user) {
      if (user) {
-
          document.getElementById('formjs').innerHTML = '';
          document.getElementById('formjs').style.display = 'none';
          document.getElementById('crud').style.display = 'block';
 
          document.getElementById('crud').innerHTML = `
     <div class = "row mt-5" >
-        <div id = "firstSection" class = "col-md-6">
-        <form id = "form" class = "border p-4 mb-4">
-        <div class = "form-group">
-        <label>Task</label><input required type = "text" class = "form-control" id = "task" placeholder = "Enter Task">
-        </div><div class = "form-group"><label>Description</label><input required type = "text" class = "form-control" id = "description" placeholder = "Description">
-        </div><p style = "display: none;" id = "taskkey"></p><button type = "button" id = "button1" class = "btn-block btn btn-primary">Add Task</button><button type = "button" id = "button2" class = "btnop2 btn btn-success" style = "display:none;" onclick = "updateTask(2)">Update Task</button><button type = "button" id = "button3" class = "btnop2 btn btn-danger" style = "display:none;" onclick = "cancel()">Cancel</button></form><h2>Fatih Çetinkaya</h2><h3>Github</h3> <
-        button type = "button" class = "btn-block btn mt-5 btn-danger" onclick = "firebase.auth().signOut()">Sign Out</button></div><div class = "col-md-6" id = "cardSection"></div><div class = "row" style = "position: fixed; bottom: 5%;"></div>
-</div>`;
+    <div id = "firstSection" class = "col-md-6">
+    <form id="form" class="border p-4 mb-4">
+
+     <div class = "form-group">
+        <label>Task</label>
+        <input type="text" class="form-control" id="task" placeholder="Enter Task" required>
+    </div>
+
+    <div class = "form-group">
+        <label>Description</label>
+        <input type="text" class="form-control" id="description" placeholder="Description" required>
+    </div>
+    
+    <p style = "display: none;" id = "taskkey"></p>
+        <button type = "submit" id = "button1" class = "btn-block btn btn-primary">Add Task</button>
+        <button type = "button" id = "button2" class = "btnop2 btn btn-success" style = "display:none;" onclick = "updateTask(2)">Update Task</button>
+        <button type = "button" id = "button3" class = "btnop2 btn btn-danger" style = "display:none;" onclick = "cancel()">Cancel</button>
+    </form>
+        <h2>Fatih Çetinkaya</h2>
+        <h3>Software Developer</h3>
+        <button type = "submit" class = "btn-block btn mt-5 btn-danger" onclick = "firebase.auth().signOut()">Sign Out</button>
+     </div>
+        <div class = "col-md-6" id = "cardSection">
+        
+        </div>
+    </div>
+
+        <div class = "row" style = "position: fixed; bottom: 5%;">
+        
+        </div>
+`;
          document.getElementById("form").addEventListener("submit", (e) => {
              e.preventDefault();
              var task = document.getElementById("task").value;
              var description = document.getElementById("description").value;
-
              createTask(task, description);
              form.reset();
+           
          });
          readTask();
      } else {
@@ -41,18 +63,24 @@
          document.getElementById("crud").innerHTML = "";
          document.getElementById("formjs").style.display = "block";
          document.getElementById("formjs").innerHTML = `
-<h1 class = "h3 mb-3 font-weight-normal">Login</h1><label for="inputEmail" class="sr-only">Email Address</label> <input type = "email" id = "inputEmail" class = "form-control mb-3" placeholder = "Email Address" required autofocus>
-    <label for = "inputPassword" class = "sr-only">Password</label><input type="password" id="inputPassword" class = "form-control mb-3" placeholder = "Password" required>
-    <button class = "btn btn-lg btn-primary btn-block"
-type = "submit">Sign in</button><p id="errorm" class="mt-5 mb-3 text-danger"></p> <p class = "mt-5 mb-3 text-muted">&copy;
-Fatih Çetinkaya < /p>`;
+<h1 class="h3 mb-3 font-weight-normal">Login</h1>
+<label for="inputEmail" class="sr-only">Email Address</label> 
+<input type = "email" id = "inputEmail" class = "form-control mb-3" placeholder = "Email Address" required autofocus />
+    <label for = "inputPassword" class = "sr-only">Password</label>
+    <input type="password" id="inputPassword" class = "form-control mb-3" placeholder = "Password" required/>
+    <button class = "btn btn-lg btn-primary btn-block" type = "submit">Sign in</button>
+<p id="errorm" class="mt-5 mb-3 text-danger"></p>
+<p class = "text-muted" > user: crud@example.com // Psw: 123456 </p>
+<p class = "mt-5 mb-3 text-muted">&copy;Fatih Çetinkaya </p>
+
+</div>`;
 
          const errorhtml = document.getElementById('errorm');
          document.getElementById('formjs').addEventListener('submit', (e) => {
              e.preventDefault();
              errorhtml.innerHTML = '';
              const email = document.getElementById('inputEmail');
-             const email = document.getElementById('inputPassword');
+             const password = document.getElementById('inputPassword');
              firebase.auth().signInWithEmailAndPassword(email.value, password.value)
                  .catch(function (error) {
                      errorhtml.innerHTML = `${error.message}`;
@@ -97,7 +125,7 @@ Fatih Çetinkaya < /p>`;
              document.getElementById('task').value = "";
              document.getElementById("description").value = "";
 
-             cancel()
+             cancel();
          });
      }
  }
@@ -105,7 +133,6 @@ Fatih Çetinkaya < /p>`;
  function cancel() {
      document.getElementById('task').value = "";
      document.getElementById('description').value = "";
-     document.getElementById('taskkey').value = id;
      document.getElementById('button1').style.display = 'block';
      document.getElementById('button2').style.display = 'none';
      document.getElementById('button3').style.display = 'none';
@@ -122,32 +149,26 @@ Fatih Çetinkaya < /p>`;
 
  function createTask(taskName, description) {
      var task = {
-
          task: taskName,
          description: description
      }
-     let db = firebase().firestore().collection("tasks/");
+     let db = firebase.firestore().collection("tasks/");
      db.add(task)
  }
 
  function readTask() {
-    firebase.firestore().collection("tasks").onSnapshot(function(snapshot) {
-        document.getElementById("cardSection").innerHTML="";
-        snapshot.forEach(function(taskValue) {
-            document.getElementById("cardSection").innerHTML=`
-            < div class = "card mb-3"
-            style = "background-color: inherit; border: 1px solid;" >
-                <
-                div class = "card-body" >
-                <
-                h5 class = "text-white card-title" >
-                Task: $ {
-                    taskValue.data().task
-                } </h5> <p class = "card-text text-white" > Description: $ {
-                    taskValue.description
+     firebase.firestore().collection("tasks").onSnapshot(function (snapshot) {
+         document.getElementById("cardSection").innerHTML = "";
+         snapshot.forEach(function (taskValue) {
+             document.getElementById("cardSection").innerHTML += `
+            <div class = "card mb-3" style="background-color: inherit; border: 1px solid;">
+                <div class = "card-body">
+                    <h5 class = "text-white card-title">
+                Task: ${taskValue.data().task}</h5>
+                 <p class="card-text text-white">Description: ${taskValue.data().description}
                 </p> <button type = "submit" class = "btnop btn btn-warning" style = "color: white;"
             onclick = "updateTask(1,'${taskValue.id}','${taskValue.data().task}','${taskValue.data().description}')" > Edit
-            Task</button> <button type = "submit" class = "btnop btn btn-danger" onclick = "deleteTask('${taskValue.id}')">Delete Task</button></div></div> `;            
-        });
-    });
+            Task</button> <button type = "submit" class = "btnop btn btn-danger" onclick = "deleteTask('${taskValue.id}')">Delete Task</button></div></div> `;
+         });
+     });
  }
